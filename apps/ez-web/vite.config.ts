1acce8343d567b1cbc0a5ee85a36d3bea124dce7
fltzr <josh.salinas.j@outlook.com>
2024-05-13 19:51:46 -0400

    chore: Update tsconfig.spec.json and vite.config.ts

--- apps/ez-web/vite.config.ts
+++ apps/ez-web/vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types='vitest' />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
@@ -65,9 +66,6 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
